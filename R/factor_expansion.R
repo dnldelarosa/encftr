5a37083b12a6fa3acbc49bba818c997f9c3d3d1e
Daniel de la Rosa @ BlackBox <drdsdaniel@gmail.com>
2026-01-03 01:35:29 -0400

    refactor: consolidar funciones de factor de expansión con deprecation warnings y agregar ftc_factor_expansion_semestre

--- R/factor_expansion.R
+++ R/factor_expansion.R
@@ -4,20 +4,27 @@
 #' @param tbl [data.frame]: Conexión a base de datos o dataframe con los datos.
 #'
 #' @return Conexión a base de datos o dataframe según input con la variable
-#'   \code{factor_exp_anual} agregada.
+#'   \code{factor_expansion_anual} agregada.
 #'
 #' @export
 #'
 #' @examples
 #'   encft <- data.frame(FACTOR_EXPANSION = c(1028, 848, 400))
-#'   ftc_factor_exp_anual(encft)
-ftc_factor_exp_anual <- function(tbl){
+#'   ftc_factor_expansion_anual(encft)
+ftc_factor_expansion_anual <- function(tbl){
   FACTOR_EXPANSION <- NULL
   tbl %>%
     dplyr::mutate(
-      factor_exp_anual = FACTOR_EXPANSION/4
+      factor_expansion_anual = FACTOR_EXPANSION/4
     )
 }
+
+
+ftc_factor_exp_anual <- function(tbl) {
+  deprecate_warn("0.5.0", "ftc_factor_exp_anual()", "ftc_factor_expansion_anual()")
+  ftc_factor_expansion_anual(tbl)
+}
+
 
 #' @rdname ftc_factor_exp_anual
 #' @export
@@ -26,3 +33,11 @@ ftc_compute_factor_exp_anual <- function(tbl) {
   ftc_factor_exp_anual(tbl)
 }
 
+
+ftc_factor_expansion_semestre <- function(tbl){
+  FACTOR_EXPANSION <- NULL
+  tbl %>%
+    dplyr::mutate(
+      factor_expansion_semestre = FACTOR_EXPANSION/2
+    )
+}
